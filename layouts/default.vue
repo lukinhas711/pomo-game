07ad8f52216a7f9c1dd4f803b13b3956d0bdd867
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2022-02-09 19:05:37 -0300

    :sparkles: feat: xp bar

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="$style.container">
+    <ExperienseBar />
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import ExperieseBar from '@/components/ExperienseBar.vue'
+
+export default Vue.extend({
+  name: 'Default',
+  components: {
+    ExperieseBar,
+  }
+})
+</script>
+
+<style lang="postcss" module>
+.container {
+  @apply min-h-screen max-w-5xl mx-auto flex flex-col px-5 py-8 md:py-10;
+}
+</style>
